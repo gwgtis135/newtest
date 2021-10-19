2cbf05d9d0ec0f41cede3ecde018cd7d049604bc
park <gwgtis135@gmail.com>
2021-10-20 00:19:00 +0900

    10/20 00:18

--- src/co/yedam/newtest/signInDAO.java
+++ src/co/yedam/newtest/signInDAO.java
@@ -4,42 +4,56 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class signInDAO extends DAO  {
+public class signInDAO extends DAO {
 
-	
-	//�떛湲��넠 諛⑹떇
+	// 싱글톤
 	private static signInDAO singleton = new signInDAO();
+
 	private signInDAO() {
-		
+
 	}
+
 	public static signInDAO getInstance() {
 		return singleton;
 	}
-	
-	public boolean checkId(String id) {
-		System.out.println("=============="+id);
-	      
-	      connect();
+
+	public user checkId(String id, String pw) {
+		System.out.println("=============sdfsdf=" + id);
+		System.out.println("==============" + pw);
+
+		connect();
+		String sql = "SELECT * FROM USERS  where user_id = ? and user_Password = ?";
 //	      List<user> list = new ArrayList<>();
-	      String sql = "SELECT * FROM USERS  where user_id = ?";
-	      
-	      try {
-	         psmt = conn.prepareStatement(sql);
-	         psmt.setString(1, id);
-	         rs = psmt.executeQuery();
-	       
-	         if ( rs.next()) {
-	            return false;
-	         } else {
-	            return true;
-	         }
-	         
-	      } catch (SQLException e) {
-	         e.printStackTrace();
-	      } finally {
-	         disconnect();
-	      }
-	      return true;
-	   }
-	
+		
+
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			psmt.setString(2, pw);
+			rs = psmt.executeQuery();
+			
+			
+			if (rs.next()) {
+				user us = new user();
+				
+				System.out.println("로그인 성공");
+				us.setUserId(rs.getString("USER_ID"));
+				us.setUserPw(rs.getString("USER_PASSWORD"));
+				us.setUserName(rs.getString("USER_NAME"));
+				return us;
+
+			} else {
+				System.out.println("로그인 실패");
+				return null;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return null;
+		
+	}
+
 }
--- src/co/yedam/newtest/signInServlet.java
+++ src/co/yedam/newtest/signInServlet.java
@@ -5,41 +5,52 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/loginServlet")
-public class loginServlet extends HttpServlet {
+@WebServlet("/signInServlet")
+public class signInServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public loginServlet() {
+    public signInServlet() {
     	
     }
   
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
 		String id = request.getParameter("userId");	//id?
+		String pw = request.getParameter("userPw");	//id?
+		//Cookie cookie = new Cookie("cname", "cvalue");
+		//response.addCookie(cookie);	//response 객체에 담기 
+		
+		
 		
 		// {"retCode":"OK"} / {"retCode":"NG"}
 				PrintWriter out = response.getWriter();
 				
 				signInDAO dao = signInDAO.getInstance();
+				user us = new user();
+				us = dao.checkId(id, pw);
 				
-				if(!(dao.checkId(id))) {
-					// �Է��� ���̵�� �ű� ����.
-					out.println("{\"retCode\":\"OK\"}");
+				if(us != null) {
+					// 입력한 아이디로 신규 생성.
+//					HttpSession session = request.getSession();
+					//session.setAttribute("user", dao.checkId(id, pw));
+
+					out.println("{\"retCode\":\"OK\",\"userId\":\""+us.getUserId()+"\",\"userName\":\""+us.getUserName()+"\"}");
+					
+					
+					
+					
 				} else {
-					// �ְų� �����߻�.
+					// 있거나 에러발생.
 					out.println("{\"retCode\":\"NG\"}");
 				}
 		
